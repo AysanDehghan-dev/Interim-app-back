acfd8e20a78fb4cd2f8f19869f495d2c69e548cc
Aysan <aysan.dehghan@icloud.com>
2025-06-01 01:43:37 +0200

    fix: Add PaginationSchema import to application_schema.py

--- app/schemas/application_schema.py
+++ app/schemas/application_schema.py
@@ -1,7 +1,7 @@
 from marshmallow import fields, validate
 
 from app.models.enums import ApplicationStatus
-from app.schemas.base import BaseSchema, EnumField, ObjectIdField, TimestampMixin
+from app.schemas.base import BaseSchema, EnumField, ObjectIdField, PaginationSchema, TimestampMixin
 
 
 class ApplicationSchema(BaseSchema, TimestampMixin):
